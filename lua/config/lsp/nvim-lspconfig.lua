73e51ccd9df6f86eddd90ab7a8753b099a57f414
mara816 <4200mati@gmail.com>
2025-06-26 21:12:24 +0200

    Uses luasnippet and removes blink

--- lua/config/lsp/nvim-lspconfig.lua
+++ lua/config/lsp/nvim-lspconfig.lua
@@ -12,7 +12,7 @@ return {
     { 'j-hui/fidget.nvim', opts = {} },
 
     -- Allows extra capabilities provided by blink.cmp
-    'saghen/blink.cmp',
+    -- 'saghen/blink.cmp',
   },
   config = function()
     -- Brief aside: **What is LSP?**
@@ -181,7 +181,7 @@ return {
     --  By default, Neovim doesn't support everything that is in the LSP specification.
     --  When you add blink.cmp, luasnip, etc. Neovim now has *more* capabilities.
     --  So, we create new capabilities with blink.cmp, and then broadcast that to the servers.
-    local capabilities = require('blink.cmp').get_lsp_capabilities()
+    -- local capabilities = require('blink.cmp').get_lsp_capabilities()
 
     -- Enable the following language servers
     --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
